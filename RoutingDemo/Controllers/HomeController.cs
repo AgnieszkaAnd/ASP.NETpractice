dc1732f9c337df6d085ebee38cd244d801acf34d
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-06-22 22:26:02 +0200

    initial status: Student class and demo table in view

---
+++ RoutingDemo/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RoutingDemo.Models;
+
+namespace RoutingDemo.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        private List<Student> students = new List<Student>() {
+                new Student(1, "John", "Kowalski", 35),
+                new Student(2, "Adam", "Smith", 19),
+                new Student(3, "Anna", "Adams", 22)
+            };
+
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        public IActionResult Students() {
+            //List<Student> students = new List<Student>() {
+            //    new Student(1, "John", "Kowalski", 35),
+            //    new Student(2, "Adam", "Smith", 19),
+            //    new Student(3, "Anna", "Adams", 22)
+            //};
+            //ViewData["students"] = students;
+
+            //ViewData["student1"] = students[0];
+            //ViewData["student2"] = students[1];
+            //ViewData["student3"] = students[2];
+
+            return View(students);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RoutingDemo/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutingDemo.Models {
+    public class Student {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Student(int id, string firstName, string lastName, int age) {
+            this.ID = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+    }
+}
